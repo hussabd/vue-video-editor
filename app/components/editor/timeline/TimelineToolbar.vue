7e78fef118f471e686a31dadb07fe5df152fc475
Dany Boza <cloud@snapmotion.one>
2026-02-05 14:12:06 -0500

    update imports

--- app/components/editor/timeline/TimelineToolbar.vue
+++ app/components/editor/timeline/TimelineToolbar.vue
@@ -23,7 +23,7 @@ import {
   IconPlayerSkipBack,
   IconPlayerSkipForward,
 } from '@tabler/icons-vue';
-import EditableTimecode from '@/components/ui/EditableTimecode.vue';
+import { EditableTimecode } from '@/components/ui/editable-timecode';
 
 const props = defineProps<{
   zoomLevel: number;
